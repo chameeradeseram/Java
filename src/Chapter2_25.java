fffc9d983d3f51981fc3885288f9357dfde39d81
chameeradeseram <43196510+chameeradeseram@users.noreply.github.com>
2018-11-09 06:33:18 +0530

    Update Chapter2_25.java

--- src/Chapter2_25.java
+++ src/Chapter2_25.java
@@ -1,8 +1,17 @@
-
+import java.util.Scanner;
 public class Chapter2_25 {
 
 	public static void main(String[] args) {
+	        Scanner n=new Scanner(System.in);
+		System.out.println("enter your number");
+		int num=n.nextInt();
 		
+		if(num%3==0){
+			System.out.println(num+ " is divisible by 3.");
+		}
+		else{
+			System.out.println(num+ " isnot divisible by 3.");
+		}	
 		
 
 	}
