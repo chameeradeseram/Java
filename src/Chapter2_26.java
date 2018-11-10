1884b4a9fe6e8bb51a2c6433d489de3abb160ce3
chameeradeseram <43196510+chameeradeseram@users.noreply.github.com>
2018-11-10 14:00:49 +0530

    Add files via upload

---
+++ src/Chapter2_26.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Chapter2_26 {
+
+	public static void main(String[] args) {
+		Scanner num=new Scanner(System.in);
+		System.out.println("enter your no1");
+		int n1=num.nextInt();
+		
+		System.out.println("enter your no2");
+		int n2=num.nextInt();
+
+	}
+
+}
---
+++ src/Chapter2_30.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+
+public class Chapter2_30 {
+
+	public static void main(String[] args) {
+		
+		Scanner n1=new Scanner(System.in);
+		System.out.println("number 1");
+		int num1=n1.nextInt();
+		
+		System.out.println("number 2");
+		int num2=n1.nextInt();
+		
+		System.out.println("number 3");
+		int num3=n1.nextInt();
+		
+		System.out.println("number 4");
+		int num4=n1.nextInt();
+		
+		System.out.println("number 5");
+		int num5=n1.nextInt();
+		
+		
+		System.out.println(num1+" " +num2+" " +num3+" " +num4+" " +num5);
+		
+
+	}
+
+}
